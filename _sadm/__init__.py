23af2626855d32a5da250f90517d6ed7cfe46186
Jeremías <jrmsdev@gmail.com>
2019-05-12 19:09:34 -0300

    fix tests

--- _sadm/__init__.py
+++ _sadm/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-from sadm import log
+from _sadm import log
 
 def build(srcdir):
 	log.debug('build')
--- t/log_test.py
+++ t/log_test.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-from sadm import log
+from _sadm import log
 
 def test_log():
-	assert log._getCaller(1) == 't/log_test.py:9'
+	assert log._getCaller(1).startswith('t/log_test.py:')
