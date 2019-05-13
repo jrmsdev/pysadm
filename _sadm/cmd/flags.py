80a5add17d9100df36d7ff16be2c3cf204908de4
Jeremías <jrmsdev@gmail.com>
2019-05-12 22:17:45 -0300

    show version info

--- _sadm/cmd/flags.py
+++ _sadm/cmd/flags.py
@@ -3,16 +3,18 @@
 
 import argparse
 
-from _sadm import log
+from _sadm import log, version
 
 def new(prog, desc = ''):
 	p = argparse.ArgumentParser(prog = prog, description = desc)
-	p.add_argument('--log', help = 'set log level', default = 'error',
-		choices = log.levels())
+	p.add_argument('-V', '--version', help = 'show version and exit',
+		action = 'version', version = version.string())
+	p.add_argument('--log', help = 'set log level',
+		default = 'error', choices = log.levels())
 	return p
 
 def parse(p):
 	args = p.parse_args()
-	# ~ print('ARGS:', args)
+	print('ARGS:', args)
 	log.init(args.log)
 	return args
