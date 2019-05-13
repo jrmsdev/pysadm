04db470f7004c95ba91c4dd871e5b42a1a0a1b2d
Jeremías <jrmsdev@gmail.com>
2019-05-13 18:19:17 -0300

    create syslog db

--- _sadm/web/syslog.py
+++ _sadm/web/syslog.py
@@ -7,6 +7,15 @@ from os import path, makedirs
 
 from _sadm import log
 
+_DB_CREATE = """
+create table syslog (
+	id integer primary key autoincrement,
+	time text default current_timestamp,
+	level text,
+	msg text
+);
+"""
+
 class _webLogger(object):
 	db = None
 
@@ -31,8 +40,9 @@ def init():
 	log.debug('init web syslog')
 	try:
 		_logger.db = _dbInit()
-	except Exception as e:
-		log.error("could not init web syslog: %s" % e)
+	except Exception as err:
+		log.debug("exception: %s" % type(err))
+		log.error("could not init web syslog: %s" % err)
 	else:
 		log.debug('attach child logger')
 		log._logger._child = _logger
@@ -48,9 +58,15 @@ _dbfile = path.expanduser('~/.local/sadm/syslog.db')
 def _dbInit():
 	log.debug("syslog init %s" % _dbfile)
 	log.debug("sqlite version %s (lib %s)" % (sqlite3.version, sqlite3.sqlite_version))
-	dbdir = path.dirname(_dbfile)
-	makedirs(dbdir, mode = 0o700, exist_ok = True)
-	return _dbCheck(sqlite3.connect(_dbfile))
+	mkdb = not path.isfile(_dbfile)
+	if mkdb:
+		dbdir = path.dirname(_dbfile)
+		makedirs(dbdir, mode = 0o700, exist_ok = True)
+	return _dbCheck(sqlite3.connect(_dbfile), mkdb)
 
-def _dbCheck(db):
+def _dbCheck(db, create):
+	if create:
+		log.debug('create syslog db')
+		db.executescript(_DB_CREATE)
+		db.commit()
 	return db
