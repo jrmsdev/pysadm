2f34b2ea68b4dd8607a788091fb163e054246ade
Jeremías <jrmsdev@gmail.com>
2019-05-16 16:12:51 -0300

    test env

---
+++ venv-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eu
+
+ENVDIR=${1:-'/opt/venv/pysadmtest'}
+PIP=${ENVDIR}/bin/pip
+
+python3 -m venv --prompt sadmtest --clear ${ENVDIR}
+
+${PIP} install -r requirements-test.txt --upgrade
+${PIP} install -r requirements.txt --upgrade
+${PIP} install -e .
+
+${PIP} list
+exit 0
