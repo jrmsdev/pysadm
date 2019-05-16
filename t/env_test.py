a3b4403189feefcb5003064a26b88a4af25a4c8c
Jeremías <jrmsdev@gmail.com>
2019-05-16 16:04:01 -0300

    test env error

--- t/env_test.py
+++ t/env_test.py
@@ -1,6 +1,8 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from pytest import raises
+from _sadm.errors import Error
 from _sadm.env import Env
 from _sadm.env.profile import Profile
 
@@ -10,3 +12,9 @@ def test_env(testing_env):
 	assert e.name == 'testing'
 	assert e.cfgfile == 'testing/config.json'
 	assert isinstance(e.profile, Profile)
+
+def test_env_error(testing_env):
+	e = testing_env
+	e.name = 'noenv'
+	with raises(Error, match = 'EnvError: noenv env not found'):
+		e._load()
