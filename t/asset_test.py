9d3d0982aa6427f2118f7f6abc89b910a6a845ae
Jeremías <jrmsdev@gmail.com>
2019-05-18 18:55:35 -0300

    test read only assets

--- t/asset_test.py
+++ t/asset_test.py
@@ -2,7 +2,10 @@
 # See LICENSE file.
 
 from pytest import raises
+
+from io import UnsupportedOperation
 from os import path
+
 from _sadm.asset import Manager
 
 def test_manager():
@@ -12,3 +15,9 @@ def test_manager():
 		fh.close()
 	with raises(FileNotFoundError):
 		m.open('nofile')
+
+def test_read_only():
+	m = Manager('tdata')
+	with raises(UnsupportedOperation, match = 'not writable'):
+		with m.open('asset-readonly.test') as fh:
+			fh.write('testing')
