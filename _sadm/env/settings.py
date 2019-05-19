c0fbe28a8c81d2dd1d094c4c6e94a8db30726619
Jeremías <jrmsdev@gmail.com>
2019-05-19 05:10:42 -0300

    test settings

--- _sadm/env/settings.py
+++ _sadm/env/settings.py
@@ -13,14 +13,12 @@ class Settings(ConfigParser):
 	_env = None
 
 	def __init__(self, profile, env):
-		super().__init__(defaults = {}, allow_no_value = False, delimiters = '=',
+		super().__init__(defaults = {}, allow_no_value = False, delimiters = ('=',),
 			comment_prefixes = ('#',), strict = True, default_section = 'default')
 		self._profile = profile
 		self._env = env
 
 	def plugins(self, action):
-		if action == 'configure':
-			raise EnvError('invalid settings plugin action: configure')
 		for p in pluginsList():
 			if self.has_section(p):
 				yield (p, getPlugin(p, action))
