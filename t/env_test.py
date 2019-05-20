c502cbae0ecfb051795af7a4dcda7e4fe46fa0f9
Jeremías <jrmsdev@gmail.com>
2019-05-20 15:40:52 -0300

    test profile dir isfile error

--- t/env_test.py
+++ t/env_test.py
@@ -38,6 +38,8 @@ def test_load_error(testing_env):
 	e = testing_env()
 	with raises(EnvError, match = 'config file not set'):
 		e._load(fn = '')
+	with raises(EnvError, match = 'profile-dir-isfile.error is not a directory'):
+		e._load(pdir = 'profile-dir-isfile.error')
 
 def test_start_end_action(testing_env):
 	e = testing_env()
