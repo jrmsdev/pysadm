43b0b77482f785645c50d1833215a62a450136cc
Jeremías <jrmsdev@gmail.com>
2019-05-21 18:48:14 -0300

    env meta.json file

--- _sadm/plugin/sadmenv/build.py
+++ _sadm/plugin/sadmenv/build.py
@@ -1,13 +1,16 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+import json
 from shutil import make_archive
+
 from _sadm.plugin.utils import builddir
 
 __all__ = ['post_build']
 
 def post_build(env):
 	_tar(env)
+	_meta(env)
 	_zip(env)
 
 def _tar(env):
@@ -21,3 +24,9 @@ def _zip(env):
 	fn = builddir.fpath(env, '.')
 	make_archive(fn, 'zip', root_dir = rdir, base_dir = '.', verbose = 1)
 	env.log("%s.zip done" % fn)
+
+def _meta(env):
+	fn = builddir.fpath(env, 'meta.json', meta = True)
+	with builddir.create(env, 'meta.json', meta = True) as fh:
+		json.dump({}, fh)
+	env.log("%s done" % fn)
