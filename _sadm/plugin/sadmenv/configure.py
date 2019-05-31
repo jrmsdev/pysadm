2f7037a02a5814b32b2129e381dca4ba626728f3
Jeremías <jrmsdev@gmail.com>
2019-05-31 19:46:35 -0300

    sadmenv cfg merge

--- _sadm/plugin/sadmenv/configure.py
+++ _sadm/plugin/sadmenv/configure.py
@@ -7,3 +7,4 @@ __all__ = ['configure']
 
 def configure(env, cfg):
 	env.debug("archive formats: %s" % [a[0] for a in shutil.get_archive_formats()])
+	env.settings.merge(cfg, 'sadmenv', ('target.dir',))
