187ddfec843f134b31b951cfc2ddc5f9ec8c1c11
Jeremías <jrmsdev@gmail.com>
2019-06-02 19:18:37 -0300

    os.pkg configure

--- _sadm/plugin/os/pkg/configure.py
+++ _sadm/plugin/os/pkg/configure.py
@@ -1,9 +1,22 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from _sadm import dist
 from _sadm.env.settings import Settings
 
 __all__ = ['configure']
 
+def _optfilter(opt):
+	dn = dist.getname()
+	l = ('update', 'install', 'remove', 'prune')
+	if opt in l:
+		return opt
+	elif opt.startswith(dn + '.'):
+		for x in l:
+			n = '.' + x
+			if opt.endswith(n):
+				return opt
+	return None
+
 def configure(env, cfg):
-	pass
+	env.settings.merge(cfg, 'os.pkg', _optfilter)
