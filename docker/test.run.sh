177e2312e9336a8751cf1a68a2409db3841982e7
Jeremías <jrmsdev@gmail.com>
2019-06-02 04:49:17 -0300

    sadmtest devel tools

--- docker/test.run.sh
+++ docker/test.run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -eu
 docker run -it --rm \
 	-e PYTHONPATH=/opt/src/sadm \
+	-v ${PWD}/docker/bin:/opt/sadm/bin \
+	-v ${PWD}/docker/etc:/etc/opt/sadm \
 	-v ${PWD}:/opt/src/sadm sadmtest $@
 exit 0
-
-	#~ -v ${PWD}/docker/bin:/opt/sadm/bin \
-	#~ -v ${PWD}/docker/etc:/etc/opt/sadm \
