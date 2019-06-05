6fa9b59e8e22ebeb2870d405b0ca197ad1449d2e
Jeremías <jrmsdev@gmail.com>
2019-06-05 14:13:37 -0300

    report actions

--- _sadm/plugin/apache/deploy.py
+++ _sadm/plugin/apache/deploy.py
@@ -12,26 +12,32 @@ def deploy(env):
 	_reload = False
 	args = env.settings.getlist('apache', 'conf.disable')
 	if len(args) > 0:
+		env.log('conf.disable')
 		_reload = True
 		scripts.run('disconf.sh', args)
 	args = env.settings.getlist('apache', 'mod.disable')
 	if len(args) > 0:
+		env.log('mod.disable')
 		_reload = True
 		scripts.run('dismod.sh', args)
 	args = env.settings.getlist('apache', 'site.disable')
 	if len(args) > 0:
+		env.log('site.disable')
 		_reload = True
 		scripts.run('dissite.sh', args)
 	args = env.settings.getlist('apache', 'conf.enable')
 	if len(args) > 0:
+		env.log('conf.enable')
 		_reload = True
 		scripts.run('enconf.sh', args)
 	args = env.settings.getlist('apache', 'mod.enable')
 	if len(args) > 0:
+		env.log('mod.enable')
 		_reload = True
 		scripts.run('enmod.sh', args)
 	args = env.settings.getlist('apache', 'site.enable')
 	if len(args) > 0:
+		env.log('site.enable')
 		_reload = True
 		scripts.run('ensite.sh', args)
 	if _reload:
