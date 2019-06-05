3d9b1d04f7c89cd3bb323043b2f5aa1b975f8e6f
Jeremías <jrmsdev@gmail.com>
2019-06-05 20:42:40 -0300

    scripts environ

--- _sadm/plugin/utils/scripts.py
+++ _sadm/plugin/utils/scripts.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-from os import path
+from os import path, environ
 from subprocess import call, TimeoutExpired
 
 from _sadm import log, libdir, dist
@@ -13,18 +13,20 @@ _TTL = 180
 
 class Scripts(object):
 	_dir = None
+	_env = None
 
 	def __init__(self, pname, distname = None):
 		if distname is None:
 			distname = dist.getname()
 		self._dir = libdir.path('scripts', distname, pname.replace('.', path.sep))
 		log.debug("%s" % self._dir)
+		self._env = environ.copy()
 
 	def run(self, script, args = tuple()):
 		spath = path.join(self._dir, script)
 		log.debug("run %s %s" % (spath, args))
 		s = _Script(spath, args)
-		return s.dispatch()
+		return s.dispatch(env = self._env)
 
 class _Script(object):
 	_cmd = None
@@ -33,9 +35,11 @@ class _Script(object):
 		self._cmd = [script]
 		self._cmd.extend(args)
 
-	def dispatch(self):
+	def dispatch(self, env = None):
+		if env is None:
+			env = environ.copy()
 		try:
-			return call(self._cmd, timeout = _TTL)
+			return call(self._cmd, env = env, timeout = _TTL)
 		except FileNotFoundError:
 			raise PluginScriptNotFound(self._cmd[0])
 		except PermissionError:
