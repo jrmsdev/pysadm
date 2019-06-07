21b309e60ccd342f23f83b5ab6e27595ffd68adf
Jeremías <jrmsdev@gmail.com>
2019-06-07 20:53:17 -0300

    sync and deploy iptables rules

--- _sadm/plugin/network/iptables/deploy.py
+++ _sadm/plugin/network/iptables/deploy.py
@@ -1,7 +1,9 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from _sadm.plugin.utils.cmd import call_check
+
 __all__ = ['deploy']
 
 def deploy(env):
-	pass
+	call_check('service netfilter-persistent reload')
