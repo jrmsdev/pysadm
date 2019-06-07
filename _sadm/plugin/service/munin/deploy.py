0550a6860ed2130c60887ebfb38a35376b65be90
Jeremías <jrmsdev@gmail.com>
2019-06-07 19:53:44 -0300

    munin config dbdir

--- _sadm/plugin/service/munin/deploy.py
+++ _sadm/plugin/service/munin/deploy.py
@@ -8,17 +8,20 @@ from _sadm.plugin.utils.cmd import call, call_check
 
 __all__ = ['deploy']
 
-_dbdir = path.join(path.sep, 'srv', 'munin', 'db')
-
 def deploy(env):
+	dbdir = env.settings.get('service.munin', 'db.dir')
+	makedirs(dbdir, mode = 0o755, exist_ok = True)
+	chmod(dbdir, 0o750)
+
+	dbuser = env.settings.get('service.munin', 'dbdir.user')
+	dbgroup = env.settings.get('service.munin', 'dbdir.group')
+	chown(dbdir, user = dbuser, group = dbgroup)
+
+	env.log("dbdir %s (%s:%s)" % (dbdir, dbuser, dbgroup))
+
 	if call('service cron status') == 0:
 		call_check('service cron reload')
 	else:
 		call_check('service cron start')
 
-	makedirs(_dbdir, mode = 0o755, exist_ok = True)
-	chown(_dbdir, user = 'munin', group = 'munin')
-	chmod(_dbdir, 0o750)
-
-	env.log("dbdir %s" % _dbdir)
 	call('service munin start')
