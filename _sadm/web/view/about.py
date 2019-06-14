521669fdd1b8932afdb217e6e3dea7b370492b06
Jeremías <jrmsdev@gmail.com>
2019-06-14 01:53:40 -0300

    show copyright year

--- _sadm/web/view/about.py
+++ _sadm/web/view/about.py
@@ -2,9 +2,11 @@
 # See LICENSE file.
 
 import sqlite3
-from sys import version as python_version
+
 from bottle import route, view
 from bottle import __version__ as bottle_version
+from datetime import date
+from sys import version as python_version
 
 from _sadm import log
 from _sadm.web import tpl
@@ -18,4 +20,5 @@ def about():
 		'pythonVersion': python_version,
 		'bottleVersion': bottle_version,
 		'sqliteVersion': "%s (lib %s)" % (sqlite3.version, sqlite3.sqlite_version),
+		'curyear': str(date.today().year),
 	}
