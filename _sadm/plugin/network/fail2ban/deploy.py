a3d620fc53893e75e29dbddad9daadf4a64a900f
Jeremías <jrmsdev@gmail.com>
2019-06-13 21:38:39 -0300

    fail2ban deploy

--- _sadm/plugin/network/fail2ban/deploy.py
+++ _sadm/plugin/network/fail2ban/deploy.py
@@ -1,7 +1,19 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-# ~ __all__ = ['deploy']
+from os import path, unlink
 
-# ~ def deploy(env):
-	# ~ pass
+from _sadm.plugin.utils.cmd import call_check
+
+__all__ = ['deploy']
+
+def deploy(env):
+	destdir = env.settings.get('network.fail2ban', 'jail.destdir')
+	jdisable = env.settings.getlist('network.fail2ban', 'jail.disable')
+	for jn in jdisable:
+		fn = path.join(destdir, jn + '.conf')
+		if path.isfile(fn):
+			env.log("remove %s" % fn)
+			unlink(fn)
+	call_check('service fail2ban restart')
+	call_check('service fail2ban status')
