495f5e817a7015f43d7d233c999c424546052db7
Jeremías <jrmsdev@gmail.com>
2019-06-14 03:52:59 -0300

    set license and version pydoc info

--- sadm.py
+++ sadm.py
@@ -1,11 +1,14 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from _sadm import version
 from _sadm.cmd import build as build_cmd
 from _sadm.cmd import deploy as deploy_cmd
 from _sadm.cmd import web as web_cmd
 
 __all__ = ['build', 'deploy', 'web']
+__license__ = 'BSD'
+__version__ = version.get()
 
 build = build_cmd.main
 deploy = deploy_cmd.main
