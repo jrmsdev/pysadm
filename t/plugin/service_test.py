77136b0d263a15fedb72aa74ec8bdae72c9b0bfc
Jeremías <jrmsdev@gmail.com>
2019-07-12 19:57:12 -0300

    relocat plugin tdata

--- t/plugin/service_test.py
+++ t/plugin/service_test.py
@@ -6,13 +6,13 @@ from os import path
 from _sadm.plugin import service
 
 def test_defaults(testing_env):
-	env = testing_env('plugin.service')
-	assert env.name() == 'plugin.service'
-	assert env.profile() == 'testing'
-	assert env.cfgfile() == path.join('plugin', 'service.ini')
+	env = testing_env(name = 'service', profile = 'plugin')
+	assert env.name() == 'service'
+	assert env.profile() == 'plugin'
+	assert env.cfgfile() == path.join('service', 'config.ini')
 	assert env.settings.sections() == []
 
 def test_configure(testing_env):
-	env = testing_env('plugin.service')
+	env = testing_env(name = 'service', profile = 'plugin')
 	env.configure()
-	assert sorted(env.settings.sections()) == ['os', 'sadm', 'service', 'testing']
+	assert sorted(env.settings.sections()) == ['sadm', 'service']
