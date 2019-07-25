ff4194553df8d722a0b6e0ca53d76beb5fd03f7d
Jeremías <jrmsdev@gmail.com>
2019-07-25 18:17:04 -0300

    (try to) avoid duplicate requirements

--- venv/mkdev.sh
+++ venv/mkdev.sh
@@ -5,8 +5,8 @@ PIP=${ENVDIR}/bin/pip
 
 python3 -m venv --prompt sadmdev --clear ${ENVDIR}
 
-${PIP} install --upgrade -r venv/requirements-dev.txt
 ${PIP} install --upgrade -r venv/requirements-test.txt
+${PIP} install --upgrade -r venv/requirements-dev.txt
 ${PIP} install --upgrade -r requirements.txt
 ${PIP} install --upgrade -e .
 
