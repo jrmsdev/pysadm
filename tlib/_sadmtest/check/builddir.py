3e3e756030e0e841e0e2728af398692e3f674748
Jeremías <jrmsdev@gmail.com>
2019-07-26 01:43:18 -0300

    check builddir root content

--- tlib/_sadmtest/check/builddir.py
+++ tlib/_sadmtest/check/builddir.py
@@ -5,9 +5,25 @@ from os import path
 
 class CheckBuilddir(object):
 	_rootdir = None
+	_deployfn = None
+	_envfn = None
+	_metadir = None
+	_zipfn = None
 
 	def __init__(self, env):
 		self._rootdir = env.build.rootdir()
+		self._deployfn = self._rootdir + '.deploy'
+		self._envfn = self._rootdir + '.env'
+		self._metadir = self._rootdir + '.meta'
+		self._zipfn = self._rootdir + '.zip'
 
 	def content(self):
 		assert path.isdir(self._rootdir), "%s builddir not found" % self._rootdir
+		assert path.isfile(self._deployfn), \
+			"%s deploy self-extract file not found" % self._deployfn
+		assert path.isfile(self._envfn), \
+			"%s deploy .env file not found" % self._envfn
+		assert path.isdir(self._metadir), \
+			"%s build meta dir not found" % self._metadir
+		assert path.isfile(self._zipfn), \
+			"%s deploy zip file not found" % self._zipfn
