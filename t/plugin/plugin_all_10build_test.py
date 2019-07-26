4e2e7834c9c9356d4221713b225f75116ab3d5ce
Jeremías <jrmsdev@gmail.com>
2019-07-26 02:15:29 -0300

    test all configured plugins build

--- t/plugin/plugin_all_10build_test.py
+++ t/plugin/plugin_all_10build_test.py
@@ -1,16 +1,23 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-# ~ from _sadm.configure import pluginsList
+from os import path
+
+from _sadm.configure import pluginsList
+
+_srcdir = path.dirname(path.dirname(path.dirname(__file__)))
 
 def test_build_testing(testing_plugin):
 	p = testing_plugin('testing', ns = '_sadmtest', cfgfn = 'config-build.ini')
 	p.build()
+	p.check.builddir.file('sadm.testing') # just to check it works both ways
 	p.check.builddir.file('sadm.testing', 'testing\n')
 
-# ~ def test_all_configure(testing_plugin):
-	# ~ for n in pluginsList():
-		# ~ if n in ('testing', 'sadm', 'sadmenv'):
-			# ~ continue
-		# ~ p = testing_plugin(n)
-		# ~ assert p.configure(), "%s plugin.configure failed" % n
+def test_all_build(testing_plugin):
+	for n in pluginsList():
+		if n == 'testing':
+			continue
+		fn = path.join(_srcdir, 'tdata', 'plugin', n, 'config-build.ini')
+		if path.isfile(fn):
+			p = testing_plugin(n, cfgfn = 'config-build.ini')
+			p.build()
