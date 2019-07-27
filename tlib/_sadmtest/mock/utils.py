3f9858f83aa8c8ca52639a32344c89d744a4d984
Jeremías <jrmsdev@gmail.com>
2019-07-26 23:31:40 -0300

    mock cmd side effect wrapper

--- tlib/_sadmtest/mock/utils.py
+++ tlib/_sadmtest/mock/utils.py
@@ -4,14 +4,26 @@
 from unittest.mock import Mock
 
 class MockCmdProc(object):
-	_main = None
+	_mock = None
+	_cfg = None
+	call = None
+	check_call = None
 
 	def __init__(self):
-		self._main = Mock()
-		self.call = self._main.mock_call
-		self.check_call = self._main.mock_check_call
+		self._mock = Mock()
+		self.call = self._mock.mock_call
+		self.check_call = self._mock.mock_check_call
 		self._configure()
 
 	def _configure(self):
-		self.call.side_effect = [0, 0, 0, 0, 0, 0]
-		self.check_call.side_effect = [0, 0]
+		self._cfg = {
+			'call': 0,
+			'check_call': 0,
+		}
+		self.call.side_effect = self._sideEffect('call')
+		self.check_call.side_effect = self._sideEffect('check_call')
+
+	def _sideEffect(self, method):
+		def wrapper(*args, **kwargs):
+			return self._cfg[method]
+		return wrapper
