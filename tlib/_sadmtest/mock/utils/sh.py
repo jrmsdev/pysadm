6d414fbb13f338755dcea5e155da5f685be09b11
Jeremías <jrmsdev@gmail.com>
2019-08-15 12:01:14 -0300

    mock shutil return values

--- tlib/_sadmtest/mock/utils/sh.py
+++ tlib/_sadmtest/mock/utils/sh.py
@@ -1,6 +1,7 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from collections import deque
 from os import path
 from unittest.mock import Mock
 
@@ -37,6 +38,7 @@ class MockTmpFile(object):
 class MockShUtil(object):
 	_mock = None
 	_expect = None
+	_return = None
 	makedirs = None
 	chmod = None
 	chown = None
@@ -53,6 +55,7 @@ class MockShUtil(object):
 		self.getcwd = self._mock.mock_getcwd
 		self.chdir = self._mock.mock_chdir
 		self._expect = []
+		self._return = {}
 		self._configure(cfg)
 
 	def _configure(self, cfg):
@@ -60,15 +63,27 @@ class MockShUtil(object):
 		self.getcwd.side_effect = self._getcwd
 		if cfg is None:
 			return
-		self._utilsExpect(cfg)
+		self._parseConfig(cfg)
 
-	def _utilsExpect(self, cfg):
+	def _parseConfig(self, cfg):
 		data = cfg.get('shutil', fallback = '')
 		if data != '':
 			for l in data.splitlines():
 				l = l.strip()
 				if l != '':
-					self._expect.append(l)
+					x = l.split(';')
+					rtrn = x[0].strip()
+					cmdline = ';'.join(x[1:]).strip()
+					self._expect.append(cmdline)
+					util = cmdline.split(' ')[0].strip()
+					self._utilReturn(util, rtrn)
+
+	def _utilReturn(self, name, data):
+		if name == '':
+			raise RuntimeError('mock shutil: util name is empty')
+		if self._return.get(name, None) is None:
+			self._return[name] = deque()
+		self._return[name].appendleft(data)
 
 	def _mktmp(self, suffix = None, prefix = None, dir = None):
 		return MockTmpFile(suffix = suffix, prefix = prefix, dir = dir)
@@ -76,7 +91,7 @@ class MockShUtil(object):
 	def _getcwd(self):
 		return path.join(path.sep, 'testing', 'workdir')
 
-	def check(self): # TODO!!
+	def check(self):
 		got = []
 		for x in self._mock.mock_calls:
 			xname = x[0].replace('mock_', '', 1)
