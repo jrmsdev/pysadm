dc44157d61da861ada0c26fbde701b4f7da38cfd
Jeremías <jrmsdev@gmail.com>
2019-08-20 18:36:15 -0300

    sadm.listen webhook plugin check context rule

--- _sadm/listen/plugin/webhook.py
+++ _sadm/listen/plugin/webhook.py
@@ -8,15 +8,20 @@ from _sadm import log
 __all__ = ['WebhookPlugin']
 
 class WebhookPlugin(object):
+	name = 'sadm.webhook'
 
 	def setup(self, wapp):
 		log.debug('setup')
 
 	def apply(self, callback, ctx):
-		log.debug('apply')
-		def wrapper(*args, **kwargs):
-			log.debug('apply.wrapper')
-			resp = callback(*args, **kwargs)
-			response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
-			return resp
-		return wrapper
+		log.debug("ctx: %s - %s" % (type(ctx), ctx))
+		if ctx['rule'].startswith('/hook/'):
+			log.debug('apply')
+			def wrapper(*args, **kwargs):
+				log.debug('apply.wrapper')
+				resp = callback(*args, **kwargs)
+				response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
+				return resp
+			return wrapper
+		log.debug("ignore rule: %s" % ctx['rule'])
+		return callback
