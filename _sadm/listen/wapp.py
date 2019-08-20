860d18fc29335b6bdc69b2ef319846ffdaef52e6
Jeremías <jrmsdev@gmail.com>
2019-08-20 12:13:05 -0300

    wapp init

--- _sadm/listen/wapp.py
+++ _sadm/listen/wapp.py
@@ -14,18 +14,23 @@ bottle.TEMPLATE_PATH = []
 _cfgfn = libdir.fpath('listen', 'wapp.conf')
 wapp.config.load_config(_cfgfn)
 
-def init():
-	_systemCfg = path.join(path.sep, 'etc', 'opt', 'sadm', 'listen.cfg')
-	if path.isfile(_systemCfg):
-		wapp.config.load_config(_systemCfg)
+def init(cfgfn = None):
+	if cfgfn is None:
+		cfgfn = path.join(path.sep, 'etc', 'opt', 'sadm', 'listen.cfg')
+	if path.isfile(cfgfn):
+		wapp.config.load_config(cfgfn)
 
 	log.init(wapp.config['sadm.log'])
 	log.debug(version.string('sadm'))
 
+	initDone = {}
+
 	for opt in wapp.config.keys():
 		if opt.startswith('sadm.webhook.'):
-			log.debug("enable %s" % opt)
-			from _sadm.listen.webhook import WebhookPlugin
-			wapp.install(WebhookPlugin())
+			if not initDone.get('webhook', False):
+				log.debug('enable webhook')
+				from _sadm.listen import webhook
+				webhook.init(wapp)
+				initDone['webhook'] = True
 
 	return wapp
--- _sadm/listen/webhook/__init__.py
+++ _sadm/listen/webhook/__init__.py
@@ -3,13 +3,7 @@
 
 from . import bitbucket
 
-__all__ = ['WebhookPlugin']
+__all__ = ['init']
 
-class WebhookPlugin(object):
-	name = 'sadm.webhook'
-
-	def setup(self, wapp):
-		pass
-
-	def apply(self, callback, route):
-		pass
+def init(wapp):
+	pass
