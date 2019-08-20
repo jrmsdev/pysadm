8f71dc94bc3785b881925a3fde4660f2778c2a38
Jeremías <jrmsdev@gmail.com>
2019-08-20 14:11:41 -0300

    sadm.listen text/plain error response

--- _sadm/listen/errors.py
+++ _sadm/listen/errors.py
@@ -1,17 +1,21 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from bottle import response
+
 from _sadm import log
 
 __all__ = ['init']
 
 def handler(code, error):
+	log.debug("handler %d" % code)
 	log.debug("%d - %s" % (error.status_code, error.status_line))
 	argsLen = len(error.args)
 	if argsLen >= 3:
 		log.error("%s" % error.args[2])
 	if argsLen >= 4:
 		log.debug("%s" % error.args[3])
+	response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
 	return "ERROR: %d" % code
 
 def init(wapp):
