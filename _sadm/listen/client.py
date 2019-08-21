667accda55b264f970b3a5faa55dc62f3c716a8a
Jeremías <jrmsdev@gmail.com>
2019-08-21 19:29:14 -0300

    listen.client post exec task

--- _sadm/listen/client.py
+++ _sadm/listen/client.py
@@ -1,6 +1,9 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+import json
+from urllib.request import urlopen
+
 from _sadm import log
 
 __all__ = ['ListenClient']
@@ -10,7 +13,17 @@ class ListenClient(object):
 
 	def __init__(self, url):
 		log.debug("url %s" % url)
-		self._url = url
+		self._url = self._path(url, '_')
+
+	def _path(self, *parts):
+		return '/'.join(parts)
+
+	def _post(self, path, data):
+		url = self._path(self._url, path)
+		with urlopen(url, data = data) as resp:
+			if resp.status != 200:
+				log.error("%s returned: %d - %s" % (url, resp.status, resp.reason))
 
 	def exec(self, task, action, args):
-		pass
+		path = self._path('exec', task, action)
+		self._post(path, json.dumps(args).encode('UTF-8'))
