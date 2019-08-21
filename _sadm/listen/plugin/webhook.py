01004e67e05505fa406663c4bdc78b875bd7c230
Jeremías <jrmsdev@gmail.com>
2019-08-21 13:42:55 -0300

    sadm.listen webhook plugin api v2

--- _sadm/listen/plugin/webhook.py
+++ _sadm/listen/plugin/webhook.py
@@ -8,19 +8,20 @@ from _sadm import log
 __all__ = ['WebhookPlugin']
 
 class WebhookPlugin(object):
+	api = 2
 	name = 'sadm.webhook'
 
 	def setup(self, wapp):
 		log.debug('setup')
 
 	def apply(self, callback, ctx):
-		if ctx['rule'].startswith('/hook/'):
-			log.debug("apply for rule: %s" % ctx['rule'])
+		if ctx.rule.startswith('/hook/'):
+			log.debug("apply for rule: %s" % ctx.rule)
 			def wrapper(*args, **kwargs):
 				log.debug('apply.wrapper')
 				resp = callback(*args, **kwargs)
 				response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
 				return resp
 			return wrapper
-		log.debug("ignore rule: %s" % ctx['rule'])
+		log.debug("ignore rule: %s" % ctx.rule)
 		return callback
