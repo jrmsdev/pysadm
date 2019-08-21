95527053b2490a75ed55ca87a53abece6d8a22e4
Jeremías <jrmsdev@gmail.com>
2019-08-21 00:34:14 -0300

    pass sadm.log arg

--- _sadm/listen/exec.py
+++ _sadm/listen/exec.py
@@ -4,7 +4,7 @@
 import json
 import sys
 
-from _sadm import log, libdir
+from _sadm import log, libdir, version
 from _sadm.utils import sh, path
 from _sadm.utils.cmd import callCheck
 
@@ -12,10 +12,15 @@ __all__ = ['dispatch']
 
 def dispatch(task, **kwargs):
 	log.debug("dispatch task: %s" % task)
+	obj = {
+		'sadm.log': log.curLevel(),
+		'task': task,
+		'args': kwargs,
+	}
 	taskfn = None
 	with sh.mktmp(prefix = __name__, suffix = ".%s.json" % task) as fh:
 		taskfn = fh.name()
-		fh.write(json.dumps({'task': task, 'args': kwargs}))
+		fh.write(json.dumps(obj))
 	try:
 		_run(taskfn)
 	finally:
@@ -32,10 +37,12 @@ def main(args):
 		print('ERROR: sadm-listen exec no args', file = sys.stderr)
 		return 1
 	taskfn = args[0]
-	print(taskfn)
 	task = None
 	with open(taskfn, 'r') as fh:
 		task = json.load(fh)
+	log.init(task.get('sadm.log', 'warn'))
+	log.debug(version.string('sadm-listen'))
+	log.debug("%s" % taskfn)
 	return 0
 
 if __name__ == '__main__':
