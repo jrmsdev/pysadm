f033d9832669d9485b456c375fcff15a806a4a31
Jeremías <jrmsdev@gmail.com>
2019-08-21 18:19:06 -0300

    docker install deps: at and uwsgi

--- docker/run.sh
+++ docker/run.sh
@@ -12,9 +12,6 @@ PYPATH=''
 if test "${TAG}" = 'dev' || test "${TAG}" = 'docs'; then
 	PYPATH='-e PYTHONPATH=/opt/src/sadm'
 fi
-docker run -it --rm --name=${NAME} \
-	--hostname=${NAME} \
-	-v ${PWD}:/opt/src/sadm \
-	--user sadm \
-	${PYPATH} ${IMAGE} $@
+docker run -it --rm --name=${NAME} --hostname=${NAME} --user sadm \
+	-v ${PWD}:/opt/src/sadm ${PYPATH} ${IMAGE} $@
 exit 0
