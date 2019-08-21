1c88ce635d76578cb32a09bb5f857f52518b6148
Jeremías <jrmsdev@gmail.com>
2019-08-21 04:00:54 -0300

    webhook.repo git pull

--- _sadm/listen/webhook/repo/vcs/git.py
+++ _sadm/listen/webhook/repo/vcs/git.py
@@ -1,9 +1,21 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from _sadm import log
+from _sadm.utils import sh
+from _sadm.utils.cmd import callCheck
+
 __all__ = ['GitRepo']
 
 class GitRepo(object):
 
 	def hook(self, action, args):
-		pass
+		repodir = args.get('repo.path', 'NOREPOPATH')
+		log.debug("hook action %s repo dir %s" % (action, repodir))
+		sh.chdir(repodir)
+		if action == 'push':
+			self._pull()
+
+	def _pull(self):
+		log.debug('git pull')
+		callCheck(['git', 'pull'])
