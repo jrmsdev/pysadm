abfdede9c11791b24c5531ecac1e25e6c1ff45b6
Jeremías <jrmsdev@gmail.com>
2019-08-28 13:22:36 -0300

    test listen exec no task set

--- t/listen/listen_exec_test.py
+++ t/listen/listen_exec_test.py
@@ -1,13 +1,42 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-from unittest.mock import Mock
+from os import path
+
+from _sadmtest import mock
 
 from _sadm.listen import exec as _exec
+from _sadm.listen import wapp
 
 def test_get_url(listen_wapp):
-	with listen_wapp() as wapp:
-		req = wapp.request()
-		assert req.url == 'http://127.0.0.1/'
-		url = _exec._getURL(req)
-		assert url == 'http://127.0.0.1:3666'
+	with mock.log():
+		with listen_wapp() as wapp:
+			req = wapp.request()
+			assert req.url == 'http://127.0.0.1/'
+			url = _exec._getURL(req)
+			assert url == 'http://127.0.0.1:3666'
+
+def test_main_no_args(listen_wapp):
+	with mock.log():
+		with mock.utils(None):
+			with listen_wapp() as wapp:
+				rc = _exec.main([])
+				assert rc == 1
+
+def test_main_no_task_file(listen_wapp):
+	cfgfn = path.join('tdata', 'listen', 'exec', 'listen.cfg')
+	cfg = wapp._newConfig(cfgfn)
+	with mock.log():
+		with mock.utils(cfg, tag = 'no_task_file'):
+			with listen_wapp():
+				rc = _exec.main(['no.task.file'])
+				assert rc == 2
+
+def test_main_no_task(listen_wapp):
+	cfgfn = path.join('tdata', 'listen', 'exec', 'listen.cfg')
+	cfg = wapp._newConfig(cfgfn)
+	with mock.log():
+		with mock.utils(cfg, tag = 'no_task'):
+			with listen_wapp():
+				rc = _exec.main(['tdata/listen/exec/unset.task'])
+				assert rc == 3
