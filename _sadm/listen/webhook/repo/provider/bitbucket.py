dc678e2b371cdb8f97d59c6e467283721eb11b30
Jeremías <jrmsdev@gmail.com>
2019-09-10 14:51:09 -0300

    test auth repo

--- _sadm/listen/webhook/repo/provider/bitbucket.py
+++ _sadm/listen/webhook/repo/provider/bitbucket.py
@@ -9,30 +9,30 @@ class BitbucketProvider(object):
 
 	def auth(self, slug, req, cfg, data):
 		for arg in ('name', 'uuid'):
-			self._authRepo(arg, slug, req, cfg, data)
+			self._authRepo(arg, slug, cfg, data)
 		for arg in ('nickname', 'uuid'):
-			self._authUser(arg, slug, req, cfg, data)
+			self._authUser(arg, slug, cfg, data)
 		return True
 
-	def _authRepo(self, opt, slug, req, cfg, data):
+	def _authRepo(self, opt, slug, cfg, data):
 		val = cfg.get("auth.%s" % opt)
-		if val != '':
+		if val:
 			try:
 				x = data['repository'][opt]
 				if val != x:
-					error(403, "%s forbidden: %s %s" % (slug, opt, x))
+					raise error(403, "%s forbidden: %s %s" % (slug, opt, x))
 			except KeyError:
-				error(403, "%s forbidden: no %s" % (slug, opt))
+				raise error(403, "%s forbidden: no %s" % (slug, opt))
 
-	def _authUser(self, opt, slug, req, cfg, data):
+	def _authUser(self, opt, slug, cfg, data):
 		val = cfg.get("auth.user.%s" % opt)
-		if val != '':
+		if val:
 			try:
 				x = data['actor'][opt]
 				if val != x:
-					error(403, "%s forbidden: user %s %s" % (slug, opt, x))
+					raise error(403, "%s forbidden: user %s %s" % (slug, opt, x))
 			except KeyError:
-				error(403, "%s forbidden: no user %s" % (slug, opt))
+				raise error(403, "%s forbidden: no user %s" % (slug, opt))
 
 	def repoArgs(self, slug, cfg, data):
 		return {
