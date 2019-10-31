3520dbcdec2eb980758c73f47b43a773c22a9247
Jeremías <jrmsdev@gmail.com>
2019-10-31 17:32:38 -0300

    devops webapp errors handler

--- _sadm/devops/wapp/wapp.py
+++ _sadm/devops/wapp/wapp.py
@@ -5,6 +5,7 @@ import bottle
 from configparser import ConfigParser, ExtendedInterpolation
 
 from _sadm import libdir, log, version
+from _sadm.devops import errors
 # ~ from _sadm.devops import errors, handlers
 from _sadm.utils import path
 
@@ -51,7 +52,7 @@ def init(cfgfn = _cfgfn):
 			log.debug("plugin uninstall %s" % p)
 			wapp.uninstall(p)
 
-	# ~ errors.init(wapp)
+	errors.init(wapp)
 	# ~ handlers.init(wapp, config)
 
 	log.debug("loaded handlers %s" % [r.name for r in wapp.routes])
