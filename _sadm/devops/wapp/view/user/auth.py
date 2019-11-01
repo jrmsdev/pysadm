7a56312776ab95cd6ca6019fdb45243d28c9b1c7
Jeremías <jrmsdev@gmail.com>
2019-11-01 18:17:32 -0300

    user login template

--- _sadm/devops/wapp/view/user/auth.py
+++ _sadm/devops/wapp/view/user/auth.py
@@ -1,7 +1,9 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from _sadm.devops.wapp.tpl import tpl
+
 __all__ = ['login']
 
 def login():
-	return 'login'
+	return tpl.parse('user/login.html')
