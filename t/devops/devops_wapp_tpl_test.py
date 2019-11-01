9f480aac00c6d85d8882446af7f365c4fc742ae3
Jeremías <jrmsdev@gmail.com>
2019-10-31 22:59:34 -0300

    test devops wapp tpl

---
+++ t/devops/devops_wapp_tpl_test.py
@@ -0,0 +1,15 @@
+# Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+# See LICENSE file.
+
+from _sadm.devops.wapp.tpl import tpl
+
+def test_init(devops_wapp):
+	wapp = devops_wapp()
+	with wapp.mock() as ctx:
+		tpl.parse('testing.html', tdata = None)
+		ctx.bottle.template.assert_called_once()
+
+def test_template():
+	t = tpl.Template('testing.html', {})
+	assert t.name == 'testing.html'
+	assert t.data == {}
