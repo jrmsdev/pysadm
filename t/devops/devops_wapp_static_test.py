80dc6c50bb63cdfd2ac15f89c23321ebf7b1549c
Jeremías <jrmsdev@gmail.com>
2019-11-01 01:23:34 -0300

    test devops webapp static response

--- t/devops/devops_wapp_static_test.py
+++ t/devops/devops_wapp_static_test.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-from bottle import HTTPError
+from bottle import HTTPError, HTTPResponse
 
 from _sadm.devops.wapp.static import static
 
@@ -14,3 +14,8 @@ def test_serve_notfound():
 	err = static.serve('notfound.css')
 	assert isinstance(err, HTTPError)
 	assert err.status_code == 404
+
+def test_default_css():
+	err = static.serve('default.css')
+	assert isinstance(err, HTTPResponse)
+	assert err.status_code == 200
