55ddfad8166f2244adfdfe02673bfadc931ad868
Jeremías <jrmsdev@gmail.com>
2019-11-01 01:43:00 -0300

    subscriptable template object

--- _sadm/devops/wapp/tpl/tpl.py
+++ _sadm/devops/wapp/tpl/tpl.py
@@ -3,6 +3,8 @@
 
 import bottle
 
+from _sadm import version
+
 __all__ = ['parse']
 
 class Template(object):
@@ -11,10 +13,21 @@ class Template(object):
 	def __init__(self, name, data):
 		self.name = name
 		self.data = data
+		self.data.update({
+			'sadm': {
+				'version': version.string('sadm'),
+			}
+		})
 
 	@property
 	def error(self):
 		return self.data.get('error', None)
+
+	def __getitem__(self, name):
+		val = self.data.get(name, None)
+		if val is None:
+			raise KeyError("template %s: %s" % (self.name, name))
+		return val
 
 def parse(name, **data):
 	return bottle.template(name, tpl = Template(name, data))
