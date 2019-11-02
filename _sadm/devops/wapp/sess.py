e170302623a53b240f8a2f3785239dd49da3d51e
Jeremías <jrmsdev@gmail.com>
2019-11-02 02:36:34 -0300

    split sessdb mod

--- _sadm/devops/wapp/sess.py
+++ _sadm/devops/wapp/sess.py
@@ -1,12 +1,10 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
-import sqlite3
-
 from secrets import token_urlsafe
 
 from _sadm import log
-from _sadm.utils import path, sh
+from _sadm.devops.wapp.sessdb import SessDB
 
 __all__ = ['init', 'WebappSession']
 
@@ -22,7 +20,7 @@ def init(config):
 		log.debug('close previous database connection')
 		_db.close()
 		_db = None
-	_db = _sessDB(config)
+	_db = SessDB(config)
 
 class WebappSession(object):
 	_id = None
@@ -36,50 +34,3 @@ class WebappSession(object):
 		if not self._id:
 			return None
 		return _db.check(self._id)
-
-_sessTable = """CREATE TABLE sess (
-	pk INTEGER PRIMARY KEY AUTOINCREMENT,
-	id TEXT NOT NULL UNIQUE,
-	user TEXT NOT NULL
-);
-"""
-_sessGet = 'SELECT pk, id, user FROM sess WHERE id = ?;'
-
-class _sessDB(object):
-	_db = None
-
-	def __init__(self, config):
-		dbdir = config.get('devops', 'session.dbdir',
-			fallback = path.join('~', '.local', 'sadm', 'devops', 'wapp'))
-		if dbdir == ':memory:':
-			self._uri = 'file:session.db?mode=memory&cache=shared'
-			self._mkdb()
-		else:
-			sh.makedirs(dbdir, exists_ok = True)
-			fn = path.join(dbdir, 'session.db')
-			with sh.lockd(dbdir):
-				if path.isfile(fn):
-					log.debug("unlink %s" % fn)
-					path.unlink(fn)
-				self._uri = "file:%s?cache=shared" % fn
-				self._mkdb()
-
-	def _connect(self):
-		log.debug('db connect')
-		self._db = sqlite3.connect(self._uri)
-
-	def _mkdb(self):
-		log.debug("create database %s" % self._uri)
-		self._connect()
-		self._db.execute(_sessTable)
-		self._db.commit()
-
-	def close(self):
-		self._db.close()
-
-	def check(self, sessid):
-		cur = self._db.execute(_sessGet, sessid)
-		row = cur.fetchone()
-		if row:
-			return dict(row)
-		return None
---
+++ _sadm/devops/wapp/sessdb.py
@@ -0,0 +1,56 @@
+# Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+# See LICENSE file.
+
+import sqlite3
+
+from _sadm import log
+from _sadm.utils import path, sh
+
+__all__ = ['SessDB']
+
+_sessTable = """CREATE TABLE sess (
+	pk INTEGER PRIMARY KEY AUTOINCREMENT,
+	id TEXT NOT NULL UNIQUE,
+	user TEXT NOT NULL
+);
+"""
+_sessGet = 'SELECT pk, id, user FROM sess WHERE id = ?;'
+
+class SessDB(object):
+	_db = None
+
+	def __init__(self, config):
+		dbdir = config.get('devops', 'session.dbdir',
+			fallback = path.join('~', '.local', 'sadm', 'devops', 'wapp'))
+		if dbdir == ':memory:':
+			self._uri = 'file:session.db?mode=memory&cache=shared'
+			self._mkdb()
+		else:
+			sh.makedirs(dbdir, exists_ok = True)
+			fn = path.join(dbdir, 'session.db')
+			with sh.lockd(dbdir):
+				if path.isfile(fn):
+					log.debug("unlink %s" % fn)
+					path.unlink(fn)
+				self._uri = "file:%s?cache=shared" % fn
+				self._mkdb()
+
+	def _connect(self):
+		log.debug('db connect')
+		self._db = sqlite3.connect(self._uri)
+
+	def _mkdb(self):
+		log.debug("create database %s" % self._uri)
+		self._connect()
+		self._db.execute(_sessTable)
+		self._db.commit()
+
+	def close(self):
+		self._db.close()
+
+	def check(self, sessid):
+		cur = self._db.execute(_sessGet, sessid)
+		row = cur.fetchone()
+		if row:
+			return dict(row)
+		return None
